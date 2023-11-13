2b92f9d73591eda58ccd2b7e60d0325fe6abdfea
kahbg <141033287+kahbg@users.noreply.github.com>
2023-11-13 10:23:18 -0300

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -1,76 +1,72 @@
-// creating an array and passing the number, questions, options, and
-answers
+// creating an array and passing the number, questions, options, and answers
 let questions = [
-{
-numb: 1,
-question: "quem foi o pintor de a noite estrelada?",
-answer: "Van Gogh",
-options: [
-"michelangelo",
-"van gogh",
-"aghata crist",
-"romero brito"
-]
-},
-{
-numb: 2,
-question: "qual o nome da artista que pintou o Abapuru",
-answer: "tracila do amaral",
-options: [
-"candino portinari",
-"vik muniz",
-"tarcila do amaral",
-"rossana paulino"
-]
-},
-{
-numb: 3,
-question: "quanto é 2+2?",
-answer: "4",
-options: [
-"1",
-"2",
-"3",
-"4"
-]
-},
-{
-numb: 4,
-question: "Quantos pares de costelas um ser humano, normalmente,
-possui?",
-answer: "12",
-options: [
-"12",
-"11",
-"14",
-"13"
-]
+  {
+    numb: 1,
+    question: "Quem foi a única pessoa na história a receber o Prêmio Nobel em áreas científicas diferentes?",
+    answer: "Marie Curiee",
+    options: [
+      "Albert Einstein",
+      "Marie Curiee",
+      "Stephen Hawking",
+      "Linus Pauling"
+    ]
+  },
+  {
+    numb: 2,
+    question: "Qual o país mais novo do mundo?",
+    answer: "Sudão do Sul",
+    options: [
+      "Timor Lestet",
+      "Montenegro",
+      "Sudão do Sul",
+      "Kosovo"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Qual o nome do evento astronômico responsável pelo dia mais longo do ano?",
+    answer: "Solstício de verão",
+    options: [
+      "Equinócio de outono",
+      "Solstício de inverno",
+      "Solstício de verão",
+      "Solstício de outono"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Quantos pares de costelas um ser humano, normalmente, possui?",
+    answer: "12",
+    options: [
+      "12",
+      "11",
+      "14",
+      "13"
+    ]
+  },
+  {
+    numb: 5,
+    question: "Quais planetas do sistema solar realizam seu movimento de rotação em sentido horário?",
+    answer: "Vênus e Urano",
+    options: [
+      "Saturno e Júpiter",
+      "Vênus e Urano",
+      "Terra e Marte",
+      "Mercúrio e Saturno"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
 
-},
-{
-numb: 5,
-question: "de que fruta é feito o chocolate?",
-answer: "cacau",
-options: [
-"banana",
-"cacau",
-"lixia",
-"goiaba"
-]
-},
-// you can uncomment the below codes and make duplicate as more as you
-want to add question
-// but remember you need to give the numb value serialize like
-1,2,3,5,6,7,8,9.....
-// {
-// numb: 6,
-// question: "Your Question is Here",
-// answer: "Correct answer of the question is here",
-// options: [
-// "Option 1",
-// "option 2",
-// "option 3",
-// "option 4"
-// ]
-// },
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
 ];
